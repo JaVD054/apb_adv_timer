7800eaf1910863f9e036472d024fa0d5de565ef0
JaVD054 <jeedu124@gmail.com>
2024-06-22 13:50:34 +0530

    tb test

--- tb/tb.sv
+++ tb/tb.sv
@@ -3,7 +3,7 @@
 module tb;
 
     // Inputs
-    logic HCLK;
+    logic HCLK = 0;
     logic HRESETn;
     logic [APB_ADDR_WIDTH-1:0] PADDR;
     logic [31:0] PWDATA;
@@ -11,7 +11,7 @@ module tb;
     logic PSEL;
     logic PENABLE;
     logic dft_cg_enable_i;
-    logic low_speed_clk_i;
+    logic low_speed_clk_i = 0;
     logic [EXTSIG_NUM-1:0] ext_sig_i;
 
     // Outputs
@@ -23,6 +23,12 @@ module tb;
     logic [3:0] ch_1_o;
     logic [3:0] ch_2_o;
     logic [3:0] ch_3_o;
+
+    always 
+        #10 HCLK = ~HCLK;
+
+    always 
+        #50 low_speed_clk_i = ~low_speed_clk_i;
 
     // Instantiate the DUT
     apb_adv_timer #(
@@ -50,6 +56,23 @@ module tb;
         .ch_3_o(ch_3_o)
     );
 
-    // Add your testbench code here
+    // create dumpfile
+    initial begin
+        $dumpfile("tb.vcd");
+        $dumpvars(0, tb);
+    end
+
+    initial begin
+        HRESETn = 1;
+        #100 HRESETn = 0;
+        #1000 PSEL = 1;
+        PENABLE = 1;
+        PWRITE = 1;
+        PADDR = 0;
+        PWDATA = {{27{1'b1}}, 5'b10001};
+        #111;
+
+
+    end
 
 endmodule
